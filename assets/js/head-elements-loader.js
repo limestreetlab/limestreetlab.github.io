5f346789d4be71f7f29c31f9c8dd0dcb798b9a0b
deanstreet <deanstreet@protonmail.com>
2020-12-08 11:23:41 -0500

    adding head-elements-loader.js

---
+++ assets/js/head-elements-loader.js
@@ -0,0 +1,20 @@
+//script to group and load commonly included head elements, required jqeury to load
+$(document).ready(function() {
+  
+  //inserting meta tags into head
+  $("head").prepend('<meta name="description" content="Dean Street, Technology&#8745;Statistics">');
+  $("head").prepend('<meta name="keywords" content="Dean Street Lab, Dean Street">');
+  $("head").prepend('<meta name="author" content="Dean Street">');
+  $("head").prepend('<meta charset="utf-8">');
+  $("head").prepend('<meta name="robots" content="max-snippet:35, index, follow">');
+  $("head").prepend('<meta name="viewport" content="width=device-width, initial-scale=1">');
+  
+  //inserting images into head
+  $("head").prepend('<link rel="icon" type="image/png" href="../assets/images/favicon.png">');
+  $("head").prepend('<link rel="apple-touch-icon" href="../assets/images/apple-touch-icon.png"></link>');
+       
+  //inserting bootstrap, it can override my own css, pending review
+  //$("head").prepend('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">');
+  //$("head").prepend('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>');
+  
+}); 
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,7 @@
 //script to include navigation bar (navbar) and its associated css in the page
 $(document).ready(function() {
   
-  //inserting css and external icon api inside head
+  //inserting css and external icon api (iconify) inside head
   $("head").append('<link rel="stylesheet" href="../assets/css/navbar.css" type="text/css">');
   $("head").append('<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>');
   //inserting navbar html to nav-menu container
