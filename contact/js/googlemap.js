36c0dcece8d1dd11ba2f4f637f721ef9753fc250
deanstreet <deanstreet@protonmail.com>
2021-04-27 04:42:37 -0400

    update

--- contact/js/googlemap.js
+++ contact/js/googlemap.js
@@ -6,7 +6,7 @@ function initMap() {
     };
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 7,
+        zoom: 8,
         center: myLatLng,
         disableDefaultUI: true,
         streetViewControl: false,
