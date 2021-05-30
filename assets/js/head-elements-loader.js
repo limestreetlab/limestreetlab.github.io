142eaadcf31d92a13afb14f6056a6b218390d4d4
deanstreet <deanstreet@protonmail.com>
2021-05-29 21:25:00 -0400

    book update

--- assets/js/head-elements-loader.js
+++ assets/js/head-elements-loader.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   
   //inserting images into head
   $("head").prepend('<link rel="icon" type="image/png" href="../assets/images/favicon.png">');
-  $("head").prepend('<link rel="apple-touch-icon" href="../assets/images/apple-touch-icon.png"></link>');
+  $("head").prepend('<link rel="apple-touch-icon" href="../assets/images/apple-touch-icon.png">');
        
   //inserting Bootstrap, it can override my own css, pending review
   //$("head").prepend('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">');
