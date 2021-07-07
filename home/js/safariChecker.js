22dae22ae94e18696a015e99b4a9611afee20e1b
deanstreet <deanstreet@protonmail.com>
2021-07-07 05:33:15 -0400

    home testing

---
+++ home/js/safariChecker.js
@@ -0,0 +1,8 @@
+//script to detect if broser is Safari, if yes then turn off CSS scroll-snapping
+$(document).ready(function() {
+  var agent = navigator.platform.toLowerCase();
+  
+  alert(agent);
+}
+);
+
