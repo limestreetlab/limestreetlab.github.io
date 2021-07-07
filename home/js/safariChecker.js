c8440548f3d7b6a7ca579036a3f4c7a9ad0e80e5
deanstreet <deanstreet@protonmail.com>
2021-07-07 06:04:23 -0400

    test

--- home/js/safariChecker.js
+++ home/js/safariChecker.js
@@ -1,8 +1,18 @@
 //script to detect if broser is Safari, if yes then turn off CSS scroll-snapping
 $(document).ready(function() {
-  var agent = navigator.platform.toLowerCase();
+  var platform = navigator.platform.toLowerCase();
+  var safari = ["iphone", "ipad", "mac"]; //keyword that should appear in navigator.platform is browser is safari
   
-  alert(agent);
+  var iOS = 0; //a boolean flag
+  for (var i = 0; i < safari.length; i++) {
+    if ( platform.includes(safari[i]) ) {
+      iOS = 1;
+    }
+  }
+
+  alert(iOS);
+
+
 }
 );
 
