fc0e720d78ff589f12a6e47652dc9c4870c2ada5
deanstreet <deanstreet@protonmail.com>
2021-07-10 09:59:17 -0400

    adding soem readme to codes

--- codes/js/readmeMarkdownRenderer.js
+++ codes/js/readmeMarkdownRenderer.js
@@ -40,6 +40,18 @@ $(document).ready(
       var readme = $.trim(contents);
       $('#octave-statistical-tests-readme').html(marked(readme)) ;
     }
+    
+    $.get('readme/rchartist.md', rchartistReadme, "text");
+    function rchartistReadme(contents) {
+      var readme = $.trim(contents);
+      $('#rchartist-readme').html(marked(readme)) ;
+    }
+    
+    $.get('readme/pychartist.md', pychartistReadme, "text");
+    function pychartistReadme(contents) {
+      var readme = $.trim(contents);
+      $('#pychartist-readme').html(marked(readme)) ;
+    }
 
     $.get('readme/sally-utility.md', sallyUtilityReadme, "text");
     function sallyUtilityReadme(contents) {
