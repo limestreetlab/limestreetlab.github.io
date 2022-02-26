fd465b046f68fff8905d1d4016c636157b3a2724
deanstreet <deanstreet@protonmail.com>
2022-02-26 00:41:13 -0500

    books

--- codes/js/readmeMarkdownRenderer.js
+++ codes/js/readmeMarkdownRenderer.js
@@ -1,72 +1,34 @@
 //script to render readme files written in markdown using marked.js
 //it works by
-// - getting the readme file in the readme folder and then callback
-// - the callback trims the file content, then render it to the relevant display area
+// - getting the readme file in the readme folder and pass it to a callback
+// - the callback renders the readme string to the relevant display area
+// - the callback uses Marked.js to render the markdown string by calling marked.parse(markdown string)
 $(document).ready(
+
   function() {
     
-    $.get('readme/chachachat.md', chachachatReadme, "text");
-    function chachachatReadme(contents) {
-      var readme = $.trim(contents);
-      $('#chachachat-readme').html(marked(readme)) ;
-    }
+    $.get('readme/chachachat.md', (readme) => $('#chachachat-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/pdf-title-parser.md', pdfTitleParserReadme, "text");
-    function pdfTitleParserReadme(contents) {
-      var readme = $.trim(contents);
-      $('#pdf-title-parser-readme').html(marked(readme)) ;
-    }
+    $.get('readme/pdf-title-parser.md', (readme) => $('#pdf-title-parser-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/id-data-downloader.md', ibDataDownloaderReadme, "text");
-    function ibDataDownloaderReadme(contents) {
-      var readme = $.trim(contents);
-      $('#ib-data-downloader-readme').html(marked(readme)) ;
-    }
+    $.get('readme/id-data-downloader.md', (readme) => $('#ib-data-downloader-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/saorio.md', saorioReadme, "text");
-    function saorioReadme(contents) {
-      var readme = $.trim(contents);
-      $('#saorio-readme').html(marked(readme)) ;
-    }
+    $.get('readme/saorio.md', (readme) => $('#saorio-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/oh-my-notes.md', ohMyNotesReadme, "text");
-    function ohMyNotesReadme(contents) {
-      var readme = $.trim(contents);
-      $('#oh-my-notes-readme').html(marked(readme)) ;
-    }
+    $.get('readme/oh-my-notes.md', (readme) => $('#oh-my-notes-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/octave-statistical-tests.md', octaveStatisticalTestsReadme, "text");
-    function octaveStatisticalTestsReadme(contents) {
-      var readme = $.trim(contents);
-      $('#octave-statistical-tests-readme').html(marked(readme)) ;
-    }
+    $.get('readme/octave-statistical-tests.md', (readme) => $('#octave-statistical-tests-readme').html(marked.parse(readme)), "text");
     
-    $.get('readme/tata.md', tataReadme, "text");
-    function tataReadme(contents) {
-      var readme = $.trim(contents);
-      $('#tata-readme').html(marked(readme)) ;
-    }
+    $.get('readme/tata.md', (readme) => $('#tata-readme').html(marked.parse(readme)), "text");
 
-    $.get('readme/sally-utility.md', sallyUtilityReadme, "text");
-    function sallyUtilityReadme(contents) {
-      var readme = $.trim(contents);
-      $('#sally-utility-readme').html(marked(readme)) ;
-    }
+    $.get('readme/sally-utility.md', (readme) => $('#sally-utility-readme').html(marked.parse(readme)), "text");
     
-    $.get('readme/excel-adf.md', excelAdfReadme, "text");
-    function excelAdfReadme(contents) {
-      var readme = $.trim(contents);
-      $('#excel-adf-readme').html(marked(readme)) ;
-    }
+    $.get('readme/excel-adf.md', (readme) => $('#excel-adf-readme').html(marked.parse(readme)), "text");
     
-    $.get('readme/binomial-tree.md', binomialTreeReadme, "text");
-    function binomialTreeReadme(contents) {
-      var readme = $.trim(contents);
-      $('#binomial-tree-readme').html(marked(readme)) ;
-    }
-
+    $.get('readme/binomial-tree.md', (readme) => $('#binomial-tree-readme').html(marked.parse(readme)), "text");
 
 
  
-    
-  });
+    }
+
+  );
