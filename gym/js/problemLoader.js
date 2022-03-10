74b52846ca597a75b4860b1dac5e9e50056f4cf8
deanstreet <deanstreet@protonmail.com>
2022-03-10 11:55:55 -0500

    books

--- gym/js/problemLoader.js
+++ gym/js/problemLoader.js
@@ -1,20 +1,27 @@
+/*
+script to load questions into the mental gym page
+problems are separated by subjects as folders and each folder contains a problem per file
+the script randomly picks a subject and then a question number
+it then load the picked question file as well as calling MathJax to parse Latex
+*/
+
 $(document).ready( function() {
   
   //list of subjects to draw the question from; elements must reflect questions directory structure
-  const questionSubject = ["probability"];
+  const questionSubject = ["calculus", "probability"];
    //total number of available questions, positions directly correspond to subjects, should be updated as questions are added
-  const questionsAvailable = [42]; //exact number of question files in each directory
+  const questionsAvailable = [1, 0]; //exact number of question files in each directory
 
     //codes hereafter need no maintenance
-    var numberOfSubjects = questionSubject.length; 
-    var subjectNumber = Math.floor(Math.random() * numberOfSubjects); //randomly picking a subject
-    var thisSubject = questionSubject[subjectNumber]; //the randomly picked subject
+    let numberOfSubjects = questionSubject.length; 
+    let subjectNumber = Math.floor(Math.random() * numberOfSubjects); //randomly picking a subject
+    let thisSubject = questionSubject[subjectNumber]; //the randomly picked subject
     
-    var questionNumber = Math.floor(Math.random() * questionsAvailable[subjectNumber]); //randomly picked question number
+    let questionNumber = Math.floor(Math.random() * questionsAvailable[subjectNumber]); //randomly picked question number
     questionNumber = questionNumber.toString(); //cast int to string
 
-    var thisQuestion = "./problems/" + thisSubject + "/" + thisSubject + "-" + questionNumber + ".html" + " #problem"; //string of filepath and the question id element
-    var thisAnswer   = "./problems/" + thisSubject + "/" + thisSubject + "-" + questionNumber + ".html" + " #solution"; //string of filepath and the answer id element
+    let thisQuestion = "./problems/" + thisSubject + "/" + thisSubject + "-" + questionNumber + ".html" + " #problem"; //string of filepath and the question id element
+    let thisAnswer   = "./problems/" + thisSubject + "/" + thisSubject + "-" + questionNumber + ".html" + " #solution"; //string of filepath and the answer id element
 
     $("#question").load(thisQuestion, function(){MathJax.typeset();});  //load the path for problem
     $("#answer").load(thisAnswer, function(){MathJax.typeset();} );  //load the path for answer and then callback MathJax.typeset() to re-typeset the LaTeX math
