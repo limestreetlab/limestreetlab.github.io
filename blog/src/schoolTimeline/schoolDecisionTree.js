bda738a258fdba70e435869a1e1ffecc3b755775
limestreetlab <limestreetlab@gmail.com>
2025-11-04 20:52:22 +0000

    blog update

--- blog/src/schoolTimeline/schoolDecisionTree.js
+++ blog/src/schoolTimeline/schoolDecisionTree.js
@@ -29,8 +29,11 @@ Highcharts.chart('decisionTreeChart', {
             keys: ['name', 'id', 'parent', 'level'],
             data: [
                 ['School decisions', '4+ results'],
-                ['Offer from Westminster', 'WUS &#10003;', '4+ results'],
-                ['No offer from Westminster', 'WUS &#10007;', '4+ results'],
+                ['Offer from Bute', 'Bute &#10003;', '4+ results'],
+                ['No offer from Bute', 'Bute &#10007;', '4+ results'],
+                ['Bute House', 'Bute House', 'Bute &#10003;', 6],
+                ['Offer from Westminster', 'WUS &#10003;', 'Bute &#10007;'],
+                ['No offer from Westminster', 'WUS &#10007;', 'Bute &#10007;'],
                 ['Westminster Under School', 'Westminster', 'WUS &#10003;', 6],
                 ['Offer from Ken Prep', 'KP &#10003;', 'WUS &#10007;'],
                 ['No offer from Ken Prep', 'KP &#10007;', 'WUS &#10007;'],
@@ -38,10 +41,7 @@ Highcharts.chart('decisionTreeChart', {
                 ['Offer from Glendower', 'GP &#10003;', 'KP &#10007;'],
                 ['No offer from Glendower', 'GP &#10007;', 'KP &#10007;'],
                 ['Glendower Prep', 'Glendower', 'GP &#10003;', 6],
-                ['Offer from Wimbledon', 'WP &#10003;', 'GP &#10007;'],
-                ['No offer from Wimbledon', 'WP &#10007;', 'GP &#10007;'],
-                ['TBD: Wimbledon or Ecole des Petits', '?', 'WP &#10003;'],
-                ['Stay at Ecole des Petits', "Ecole des Petits", 'WP &#10007;']
+                ['Stay at Ecole des Petits', "Ecole des Petits", 'GP &#10007;']
                 
             ]
         }
